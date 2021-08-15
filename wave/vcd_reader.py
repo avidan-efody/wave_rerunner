250e5235e896b5caf286c9e18ad298b036dbdc6d
Avidan Efody <avidan.efody@intel.com>
2021-08-15 08:06:15 -0700

    re-implementing vcd reader, still doesn't support unpacked vectors i.e. wire [31:0] paddr

--- wave/vcd_reader.py
+++ wave/vcd_reader.py
@@ -2,116 +2,138 @@ import os
 import sys
 import re
 
+# wip: better structured vcd reader
 from wave.reader_base import ReaderBase
 
 class VcdReader(ReaderBase):
     def __init__(self, replay_blocks, wave_file, excluded_sigs, inputs_only):
         self.vcd_path = wave_file #self.check_file_names_are_valid(wave_file)
+        self.excluded_sigs = excluded_sigs
+        self.replay_blocks = replay_blocks
+
+        self.scope_begin_match = r"^\$scope\s*([^ ]+)\s*([^ ]+)\s*\$end"
+        self.scope_end_match = r"\$upscope\s*\$end"
+        self.definitions_end_match = r"\$enddefinitions.*"
+
+        self.signal_match = r"\$var.*\$end"
+        self.simple_sig_match = r"([^ ]+)\s*([a-zA-Z0-9_]+)\s*\$end"
+        self.vector_sig_match = r"([^ ]+)\s*([a-zA-Z0-9_]+)\s*(\[[0-9]+\:[0-9]+\]*)\s*\$end"
+
+        self.dumpvars_match = r"^\$dumpvars.*"
+        self.new_time_match = r"^#([0-9]+)"
+        self.sig_value_match = r"^[b]{0,1}([zx10]+)[ ]{0,1}(.*)"
+
         super().__init__(replay_blocks, wave_file, excluded_sigs, inputs_only)
 
-    def extract_lines_from_vcd_file(self, valid_path):
-        f = open(valid_path, "r")
-        wave_str_block = f.readlines()
-        f.close()
-        return wave_str_block
-
     def extract_values_from_wave(self, replay_blocks, excluded_sigs, inputs_only):
-        def extract_scope_module_name(scope_hier):
-            scope_s = scope_hier.split('.')
-            if '.' in scope_hier:
-                # print (scope_s)
-                if '' == scope_hier.split('.')[-1]:
-                    scope_h = scope_hier.split('.')[-2]
+        self.sigs_values = {}
+
+        self.extract_vcd_file(self.vcd_path)
+
+        return self.sigs_values
+
+    def extract_vcd_file(self, valid_path):
+        with open(valid_path, "r") as vcd_file:
+            self.sig_name_2_vcd_name = {}
+            self.vcd_name_2_sig_name = {}
+            self.extract_scopes(vcd_file)
+            print(self.sig_name_2_vcd_name)
+            self.extract_sig_values(vcd_file)
+
+    def extract_scopes(self, vcd_file, current_scope=""):
+        while vcd_file:
+            vcd_line = vcd_file.readline()
+            if (vcd_line == "" ):
+                return
+
+            scope_begin = re.match(self.scope_begin_match, vcd_line)
+            scope_end = re.match(self.scope_end_match, vcd_line)
+            sig = re.match(self.signal_match, vcd_line)
+            simple_sig = re.search(self.simple_sig_match, vcd_line)
+            vector_sig = re.search(self.vector_sig_match, vcd_line)
+            definitions_end = re.search(self.definitions_end_match, vcd_line)
+
+            if definitions_end:
+                if (current_scope != ""):
+                    raise ValueError("Definitions terminated in the middle of scope: ", current_scope)
+                return
+
+            if scope_begin:
+                new_scope_name = scope_begin.group(2)
+                prefix = current_scope + "." if current_scope != "" else ""
+                print("going into scope: ", prefix + new_scope_name)
+                self.extract_scopes(vcd_file, prefix + new_scope_name)
+
+            if scope_end:
+                print("going out of scope: ", current_scope)
+                return
+
+            if current_scope not in self.replay_blocks:
+                # just read the lines
+                continue
+
+            if sig and simple_sig:
+                self.update_sig_tables(current_scope, simple_sig)
+
+            if sig and vector_sig:
+                self.update_sig_tables(current_scope, vector_sig)
+
+
+            if (not (vector_sig or simple_sig or scope_begin or scope_end)):
+                pass
+                #print("Unsupported line: ", vcd_line)
+
+    def update_sig_tables(self, current_scope, signal_match):
+        vcd_name = signal_match.group(1)
+        sig_name = signal_match.group(2)
+
+        full_sig_name = current_scope + "." + sig_name
+        print(full_sig_name)
+
+        if full_sig_name in self.excluded_sigs:
+            return
+
+        self.sig_name_2_vcd_name[full_sig_name] = vcd_name
+        self.vcd_name_2_sig_name[vcd_name] = full_sig_name        
+
+
+    def extract_sig_values(self, vcd_file):
+        for sig_name in self.sig_name_2_vcd_name.keys():
+            self.sigs_values[sig_name] = []
+
+        dumpvars_found = False
+
+        while vcd_file:
+            vcd_line = vcd_file.readline()
+            if (vcd_line == "" ):
+                return
+
+            dumpvars = re.match(self.dumpvars_match, vcd_line)
+            new_time = re.match(self.new_time_match, vcd_line)
+            sig_value = re.match(self.sig_value_match, vcd_line)
+
+            if dumpvars:
+                dumpvars_found = True
+                current_time = 0
+
+            if not dumpvars_found:
+                continue
+
+            if new_time:
+                current_time = new_time.group(1)
+
+            if sig_value:
+                signal_value = sig_value.group(1)
+                signal_vcd_name = sig_value.group(2)
+                if signal_vcd_name in self.vcd_name_2_sig_name:
+                    signal_name = self.vcd_name_2_sig_name[signal_vcd_name]
+                    self.sigs_values[signal_name].append((int(current_time), signal_value))
                 else:
-                    scope_h = scope_hier.split('.')[-1]
-            else:
-                scope_h = scope_hier
-            # print (scope_h)
-            return scope_h
+                    # we currently get here for expended vector signals (i.e. [x[3]])
+                    continue 
 
-        def scope_finder(vcd_str_b, scope_hier):
-            scope_index = None
-            found_hier = 0
-            hierarchy = ""
-            scope_h = extract_scope_module_name(scope_hier)
-            for line in vcd_str_b:
-                if "$scope" in line:
-                    line_s = line.split(" ")
-                    if hierarchy == "":
-                        hierarchy += line_s[-2]
-                    else:
-                        hierarchy += "." + line_s[-2]
-                    if scope_h in line:
-                        found_hier = 1
-                        scope_index = vcd_str_b.index(line)
-                        break
-            if found_hier == 0 or scope_index is None:
-                print("Error wrong scope name, Please Fix and rerun")
-                exit()
-            #    print (vcd_str_b[scope_index])
-            print('signals extract from hierarchy - ' + hierarchy)
-            return hierarchy, scope_index
 
-        # scope_index holds the line # in which the scope starts
-        def var2dict(vcd_str_b, scope_index):
-            var_dict = {}
-            index = 1
-            while "$var" in vcd_str_b[scope_index + index]:
-                var_s = vcd_str_b[scope_index + index].split()
-                if ":" in var_s[-2]:
-                    var_dict[var_s[-4]] = var_s[-3]
-                else:
-                    var_dict[var_s[-3]] = var_s[-2]
-                index += 1
-            print('signals VCD symbols dictionary:')
-            print(var_dict)
-            return var_dict
 
-        def singlas_record2hash(vcd_str_b, hierarchy, var_dict):
-            current_time = 0
-            signals_hash = {}
-            start_hash_record = 0
-            for line in vcd_str_b:
-                m = re.search("^#", line)
-                if m:
-                    current_time = line.replace("#", "")
-                    # print (current_time)
-                else:
-                    if start_hash_record:
-                        for key in var_dict.keys():
-                            if "end" not in line:
-                                if key in line:
-                                    cureent_key = hierarchy + "." + var_dict[key]
-                                    # print (cureent_key)
-                                    if cureent_key not in signals_hash.keys():
-                                        signals_hash[cureent_key] = list()
-                                    # print (line.replace(key,""))
-                                    new_item = int(current_time), line.replace(key, "").replace(" ", "").replace("b","")
-                                    signals_hash[cureent_key].append(new_item)
-                                    # print (new_item)
-                if "dumpvars" in line:
-                    start_hash_record = 1
-            # print (signals_hash)
-            return signals_hash
+            
 
-        def extract_hash_from_wave_str(vcd_list, scope_hier, excluded_sigs):
-            vcd_j = "".join(vcd_list)
-            vcd_s = vcd_j.split("\n")
-            hierarchy, scope_index = scope_finder(vcd_s, scope_hier)
-            sig_list = var2dict(vcd_s, scope_index)
-            for sig in excluded_sigs:
-                print("Removing excluded sig: ", sig, " from the list of signals to get from vcd")
-                sig_list.pop(sig, None)         
-            signals_h = singlas_record2hash(vcd_s, hierarchy, sig_list)
-            return signals_h
-
-        # scope = self.vcd.scope_by_name(replay_block)
-        vcd_str = self.extract_lines_from_vcd_file(self.vcd_path)
-        signal_values = {}
-
-        for replay_block in replay_blocks:
-            signal_values.update(extract_hash_from_wave_str(vcd_str, replay_block, excluded_sigs))
-
-        print('signals hash:')
-        print(signal_values)
-        return signal_values
+        
